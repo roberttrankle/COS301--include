556f70fdcc7c12c4737c21935ddb15c78abac58d
Bernard van Tonder <Bernardvt@users.noreply.github.com>
2017-08-16 12:00:09 +0200

    Added safety for waiting for sources, Proportional symbol support, improved readability

--- Source/CGIS-DP/FactoryPatterns/MapDesign.js
+++ Source/CGIS-DP/FactoryPatterns/MapDesign.js
@@ -1,148 +1,197 @@
-var MapDesign = function()
-{
-	this.featureToDisplay = 'ha_dwellin';//must set with UI
-	this.vectorSource;
-	this.wardsSource;
-	this.tileLayer;
-	this.wardsVectorLayer = { layer : 'value'};
-	this.featureLayer = { layer : 'value'};
-	this.symbolLayer;
-	this.map;
-	this.mapDisplayType;
-	this.defaultMap;
-	this.dotDensity;
-	this.chloro;
-	this.heatmap;
-	this.vectorLayerClasses = [];
-	this.colorPerClass = [];
-	this.propsymbol;
-	this.loadSources();
+var MapDesign = function() {
+    this.featureToDisplay = 'ha_dwellin'; //must set with UI
+    this.vectorSource;
+    this.wardsSource;
+    this.symbolSource;
+    this.tileLayer;
+    //Layers are made object (not primitives) to pass as reference to other classes
+    this.wardsVectorLayer = { layer: 'value' };
+    this.featureLayer = { layer: 'value' };
+    this.symbolLayer = { layer: 'value' };
+    this.map;
+    this.mapDisplayType;
+    this.defaultMap;
+    this.dotDensity;
+    this.chloro;
+    this.heatmap;
+    this.vectorLayerClasses = [];
+    this.colorPerClass = [];
+    this.propsymbol;
+    this.loadSources();
 };
 
 MapDesign.prototype.standardise = function() {
-	//Code to call strategy method
+    //Code to call strategy method
 };
 
 MapDesign.prototype.classify = function() {
-	//Code to classify the data
+    //Code to classify the data
 };
 
 MapDesign.prototype.render = function() {
-	//render code
+    //render code
 };
 
-MapDesign.prototype.getVectorSource = function(){
-	return this.vectorSource;
+MapDesign.prototype.getVectorSource = function() {
+    return this.vectorSource;
 };
 
-MapDesign.prototype.getWardsSource = function(){
-	return this.wardsSource;
+MapDesign.prototype.getWardsSource = function() {
+    return this.wardsSource;
 };
 
-MapDesign.prototype.setVectorSource = function(source){
-	this.vectorSource = source;
+MapDesign.prototype.setVectorSource = function(source) {
+    this.vectorSource = source;
 };
 
-MapDesign.prototype.setWardsSource = function(source){
-	this.wardsSource = source;
+MapDesign.prototype.setWardsSource = function(source) {
+    this.wardsSource = source;
 };
 
-MapDesign.prototype.loadSources= function() {
-	if(this.vectorSource == undefined)
-	{
-		this.createVectorSource();//Put in the data set as param, using default for now.
-	}
-	if(this.wardsSource == undefined)
-	{
-		this.creatwardsSource();
-	}
-	this.createWardLayer();
-	this.createFeatureLayer();
+MapDesign.prototype.loadSources = function() {
 	this.initializeMap();
-	//this.createTileLayer();
-	//this.map.addLayer(this.wardsVectorLayer);
-	//this.map.addLayer(this.featureLayer);
-	if (this.vectorSource.getState() != 'ready'){
-		this.vectorSource.on('change', function(evt) {
-	        var source = evt.target;
-	        var mapToCreate;
-	        if (source.getState() == 'ready') {
-				this.defaultMap = new defaultMapCreator();
-				this.dotDensity = new DotDensityCreator();
-				this.chloro = new chloroplethCreator();
-				this.heatmap = new HeatMapCreator();
-				this.propsymbol = new PropSymbolCreator();
-	        }
-	    });
-	} else {
-		if(this.defaultMap == undefined) {
-			this.defaultMap = new defaultMapCreator();
-			this.dotDensity = new DotDensityCreator();
-			this.chloro = new chloroplethCreator();
-			this.heatmap = new HeatMapCreator();
-			this.propsymbol = new PropSymbolCreator();
-		}
-		
-	}
-};
+    if (this.vectorSource == undefined) {
+        this.createVectorSource(); //Put in the data set as param, using default for now.
+    }
+    if (this.wardsSource == undefined) {
+        this.creatwardsSource();
+    }
+    this.createWardLayer();
+    this.createFeatureLayer();
+    //this.createTileLayer();
+    //this.map.addLayer(this.wardsVectorLayer);
+    //this.map.addLayer(this.featureLayer);
+    if (this.vectorSource.getState() != 'ready') {
+        this.vectorSource.on('change', function(evt) {
+            var source = evt.target;
+            var mapToCreate;
+            if (source.getState() == 'ready') {
+                this.defaultMap = new defaultMapCreator();
+                this.dotDensity = new DotDensityCreator();
+                this.chloro = new chloroplethCreator();
+                this.heatmap = new HeatMapCreator();
+                this.propsymbol = new PropSymbolCreator();
+            }
+        });
+    } else {
+        if (this.defaultMap == undefined) {
+            this.defaultMap = new defaultMapCreator();
+            this.dotDensity = new DotDensityCreator();
+            this.chloro = new chloroplethCreator();
+            this.heatmap = new HeatMapCreator();
+            this.propsymbol = new PropSymbolCreator();
+        }
 
-MapDesign.prototype.isSourceReady= function(val) {
-	if (this.vectorSource.getState() == 'ready') {
-		this.createWardLayer();
-		this.removeLayers();
-		this.calculateVectorClasses();
-		this.calculateColorClass();
-		switch (val) {
-	    case 0:
-	        this.defaultMap.createMap(this.map);
-	        break;
-	    case 1:
-	    	this.loadWards();
-	        this.dotDensity.createMap(this.map, this.vectorSource, this.colorPerClass, this.featureToDisplay, this.featureLayer);
-	        break;
-	    case 2:
-	    	this.loadWards();
-	    	this.heatmap.createMap(this.map, this.vectorSource, this.featureLayer);
-	        break;
-	    case 3:
-	    	this.loadWards();
-	        this.chloro.createMap(this.map, this.vectorSource, this.wardsSource, this.colorPerClass, 5, this.featureToDisplay, this.wardsVectorLayer, this.featureLayer);
-	        break;
-	    case 4:
-	    	this.loadWards();
-	        this.propsymbol.createMap();
-	        break;
-	    }
-
-	    this.map.addLayer(this.wardsVectorLayer.layer);
-
-	    return true;
-    } else{
-    	console.log("Source was not ready, map creation needs to wait.");
-    	return false;
     }
 };
 
-MapDesign.prototype.removeLayers = function()
-{
-	 if(this.wardsSource != undefined)
-	 {
-	 	console.log("removing Style");
-	 	var boundriesGeometry = this.wardsSource.getFeatures();
-	 	 for (var i = 0; i < boundriesGeometry.length; i++) {
-	 	 	boundriesGeometry[i].setStyle();
-	 	 }
-	 }
-	this.map.removeLayer(this.featureLayer.layer);
-	this.map.removeLayer(this.wardsVectorLayer.layer);
+MapDesign.prototype.isSourceReady = function(mapType) {
+    if (this.vectorSource.getState() == 'ready') {
+        this.createWardLayer();
+        this.removeLayers();
+        this.calculateVectorClasses();
+        this.calculateColorClass();
+        // if not default map loadWards
+        if (mapType != 0) {
+        	this.loadWards();
+        	// this.createWardLayer();
+        	this.createVectorSource();
+        	this.createFeatureLayer();
+         }
+        // if wardsSource || vectorSource not complete :
+        // recursiveCreate(mapType);
+        this.recursiveCreate(mapType);
+        // switch (mapType) {
+        //     case 0:
+        //         this.defaultMap.createMap(this.map);
+        //         break;
+        //     case 1:
+        //         this.loadWards();
+        //         this.dotDensity.createMap(this.map, this.vectorSource, this.colorPerClass, this.featureToDisplay, this.featureLayer);
+        //         break;
+        //     case 2:
+        //         this.loadWards();
+        //         this.heatmap.createMap(this.map, this.vectorSource, this.featureLayer);
+        //         break;
+        //     case 3:
+        //         this.loadWards();
+        //         this.chloro.createMap(this.map, this.vectorSource, this.wardsSource, this.colorPerClass, 5, this.featureToDisplay, this.wardsVectorLayer, this.featureLayer);
+        //         break;
+        //     case 4:
+        //         this.loadWards();
+        //         // Recursively run test function
+        //         this.propsymbol.createMap(this.map, this.vectorSource, this.wardsSource, this.symbolSource, this.colorPerClass, this.featureToDisplay, this);
+        //         break;
+        // }
+
+        // this.map.addLayer(this.wardsVectorLayer.layer);
+
+        return true;
+    } else {
+        console.log("Source was not ready, map creation needs to wait.");
+        return false;
+    }
 };
 
-MapDesign.prototype.createVectorSource = function(){
-	this.vectorSource = new ol.source.Vector({
+//Waits till features ready before creating
+MapDesign.prototype.recursiveCreate = function(mapType) {
+	if (mapType != 0 && (this.wardsSource == undefined || this.vectorSource == undefined || this.wardsSource.getFeatures().length <= 0 || this.vectorSource.getFeatures().length <= 0)) {
+		// console.log("A source is not ready, waiting 1s and retrying");
+		// console.log("WSc=" + this.wardsSource.getFeatures().length +  "VSc=" + this.vectorSource.getFeatures().length);
+		var that = this;
+		setTimeout(function() { 
+			that.recursiveCreate(mapType);
+		}, 1000);
+		return;
+	}
+    switch (mapType) {
+        case 0:
+            this.defaultMap.createMap(this.map);
+            break;
+        case 1:
+            this.dotDensity.createMap(this.map, this.vectorSource, this.colorPerClass, this.featureToDisplay, this.featureLayer);
+            break;
+        case 2:
+            this.heatmap.createMap(this.map, this.vectorSource, this.featureLayer);
+            break;
+        case 3:
+            this.chloro.createMap(this.map, this.vectorSource, this.wardsSource, this.colorPerClass, 5, this.featureToDisplay, this.wardsVectorLayer, this.featureLayer);
+            break;
+        case 4:
+            // Recursively run test function
+            this.propsymbol.createMap(this.map, this.vectorSource, this.wardsSource, this.symbolSource, this.colorPerClass, this.featureToDisplay, this, this.symbolLayer);
+            break;
+    }
+    this.map.addLayer(this.wardsVectorLayer.layer);
+}
+
+MapDesign.prototype.removeLayers = function() {
+    if (this.wardsSource != undefined) {
+        // console.log("removing Style");
+        var boundriesFeatures = this.wardsSource.getFeatures();
+        for (var i = 0; i < boundriesFeatures.length; i++) {
+            boundriesFeatures[i].setStyle();
+        }
+    }
+    if (this.symbolSource != undefined) {
+        //TODO clear features
+        var symbolFeatures = this.symbolSource.getFeatures();
+           for (var i = 0; i < symbolFeatures.length; i++) {
+               symbolFeatures[i].setStyle();
+           }
+       symbolSource.clear();
+    }
+    this.map.removeLayer(this.symbolLayer.layer);
+    this.map.removeLayer(this.featureLayer.layer);
+    this.map.removeLayer(this.wardsVectorLayer.layer);
+};
+
+MapDesign.prototype.createVectorSource = function() {
+    this.vectorSource = new ol.source.Vector({
         format: new ol.format.GeoJSON(),
         url: function(extent) {
             return 'http://localhost:8080/geoserver/wfs?service=WFS&' +
-                'version=1.1.0&request=GetFeature&typename=GIS:households&' +
+                'version=1.1.0&request=GetFeature&typename=Given:copc_households&' +
                 'outputFormat=application/json&srsname=EPSG:4326&maxFeatures=1000&' +
                 'bbox=' + extent.join(',') + ',EPSG:4326';
         },
@@ -151,11 +200,11 @@ MapDesign.prototype.createVectorSource = function(){
 };
 
 MapDesign.prototype.creatwardsSource = function() {
-	this.wardsSource = new ol.source.Vector({
+    this.wardsSource = new ol.source.Vector({
         format: new ol.format.GeoJSON(),
         url: function(extent) {
             return 'http://localhost:8080/geoserver/wfs?service=WFS&' +
-                'version=1.1.0&request=GetFeature&typename=GIS:wards&' +
+                'version=1.1.0&request=GetFeature&typename=Given:electoralwardsfortsh&' +
                 'outputFormat=application/json&srsname=EPSG:4326&' +
                 'bbox=' + extent.join(',') + ',EPSG:4326';
         },
@@ -163,8 +212,8 @@ MapDesign.prototype.creatwardsSource = function() {
     });
 };
 
-MapDesign.prototype.loadWards = function(){
-	this.wardsVectorLayer.layer = new ol.layer.Vector({
+MapDesign.prototype.loadWards = function() {
+    this.wardsVectorLayer.layer = new ol.layer.Vector({
         source: this.wardsSource,
         style: new ol.style.Style({
             fill: new ol.style.Fill({
@@ -189,16 +238,17 @@ MapDesign.prototype.loadWards = function(){
 }
 
 MapDesign.prototype.createFeatureLayer = function() {
-	this.featureLayer.layer = new ol.layer.Vector({
+    this.featureLayer.layer = new ol.layer.Vector({
         source: this.vectorSource,
         style: new ol.style.Style({
             visible: false
         })
     });
+    this.map.addLayer(this.featureLayer.layer);
 };
 
 MapDesign.prototype.createWardLayer = function() {
-	this.wardsVectorLayer.layer = new ol.layer.Vector({
+    this.wardsVectorLayer.layer = new ol.layer.Vector({
         source: this.wardsSource,
         style: new ol.style.Style({
             visible: false
@@ -211,15 +261,15 @@ MapDesign.prototype.createWardLayer = function() {
 //	this.map.addLayer(this.tileLayer);
 //};
 
-MapDesign.prototype.createSymbolLayer = function(){
-	var symbolSource = new ol.source.Vector({});
-    this.symbolLayer = new ol.layer.Vector({
+MapDesign.prototype.createSymbolLayer = function() {
+    var symbolSource = new ol.source.Vector({});
+    this.symbolLayer.layer = new ol.layer.Vector({
         source: symbolSource
     });
 };
 
-MapDesign.prototype.initializeMap = function(){
-	this.map = new ol.Map({
+MapDesign.prototype.initializeMap = function() {
+    this.map = new ol.Map({
         layers: [],
         target: 'map',
         controls: ol.control.defaults({
@@ -235,24 +285,24 @@ MapDesign.prototype.initializeMap = function(){
     });
 };
 
-MapDesign.prototype.setMapDisplayType= function(val) {
-	this.mapDisplayType = val;
+MapDesign.prototype.setMapDisplayType = function(mapType) {
+    this.mapDisplayType = mapType;
 };
 
 MapDesign.prototype.getMap = function() {
-	return this.map;
+    return this.map;
 }
 
 MapDesign.prototype.getVectorClasses = function() {
-	return this.vectorLayerClasses;
+    return this.vectorLayerClasses;
 }
 
 MapDesign.prototype.calculateVectorClasses = function() {
-	// this.vectorSource.forEachFeature(function(feature) {
- //    if (this.vectorLayerClasses.indexOf(feature.get(this.featureToDisplay)) == -1) {
-	//         this.vectorLayerClasses.push(feature.get(this.featureToDisplay));
-	//     }
-	// });
+    // this.vectorSource.forEachFeature(function(feature) {
+    //    if (this.vectorLayerClasses.indexOf(feature.get(this.featureToDisplay)) == -1) {
+    //         this.vectorLayerClasses.push(feature.get(this.featureToDisplay));
+    //     }
+    // });
 }
 
 MapDesign.prototype.calculateColorClass = function() {
