ada34f0084da3320c505bae96edb2509bcef628b
Bernard van Tonder <Bernardvt@users.noreply.github.com>
2017-08-19 11:23:12 +0200

    Added requesting and parsing a list of available databases

--- Source/CGIS-DP/map.js
+++ Source/CGIS-DP/map.js
@@ -6,14 +6,14 @@
     wardPrevLoaded = 1;
 }*/
 var mapDesign;
-function loadMap(val){ 
+function loadMap(mapType){ 
   if(mapDesign == undefined)
   {
     mapDesign = new MapDesign();
     console.log("DEFINE MAPDESIGN");
   }
   mapDesign.removeLayers();
-  while (mapDesign.isSourceReady(val) == false) {}
+  while (mapDesign.isSourceReady(mapType) == false) {}
 }
 
 function loadDataset(url, callback){
@@ -33,11 +33,11 @@ function changeDataset(val){
     {
         loadDataset('http://localhost:8080/geoserver/wfs?service=WFS&' +
           'version=1.1.0&request=DescribeFeatureType&typename=GIS:households&' +
-          'outputFormat=application/json&srsname=EPSG:4326&maxFeatures=1', changeAtt);
+          'outputFormat=application/json&srsname=EPSG:4326&maxFeatures=1', changeAttributes);
     }
 }
 
-function changeAtt(feat){
+function changeAttributes(feat){
     var obj = feat.responseText.substring(feat.responseText.indexOf("[{\"name\""),feat.responseText.lastIndexOf("}]}"));
     var arr = JSON.parse(obj);
     for(var i = 1; i < arr.length; i++)
@@ -55,6 +55,41 @@ function changeAtt(feat){
         }
     }
 }
+
+// Receive the getCapabilities request
+function receiveDataSets(url, callback){
+    var feat;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+            callback(this);
+        }
+    };
+    xmlHttp.open("GET", url,true);
+    xmlHttp.send(null);
+}
+
+// Request capabilities which contain the names of datasets
+function requestCapabilities(){
+    receiveDataSets('http://localhost:8080/geoserver/wfs?service=WFS&version=1.1.0&request=GetCapabilities&outputFormat=application/json',
+        listDataSets);
+}
+
+//This uses jQuery which is to be added in the html :  
+function listDataSets(response){
+    var obj = response.responseText;
+    var featureDatabases = []
+    var xmlDoc = $.parseXML(obj);
+    var $xml = $(xmlDoc);
+    var $featureDB = $xml.find("FeatureType");
+    $featureDB.each(function(){
+        featureDatabases.push($(this).find("Name").text());
+    });
+    console.log(featureDatabases);
+    return featureDatabases;
+}
+
+
 /* Citations
   A: https://gis.stackexchange.com/questions/167112/how-to-create-a-circle-in-openlayers-3
   B: https://stackoverflow.com/questions/25924762/is-it-possible-to-identify-all-the-feature-layer-inside-of-other-layer
