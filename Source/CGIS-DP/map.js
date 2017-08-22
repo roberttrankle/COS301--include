e667469e2b2b1e68ebbc7cbe39f7669b2316a6b5
Robert Trankle <rstrankle1@gmail.com>
2017-08-22 12:59:54 +0200

    Added function for Wizard Map Creation

--- Source/CGIS-DP/map.js
+++ Source/CGIS-DP/map.js
@@ -6,15 +6,41 @@
     wardPrevLoaded = 1;
 }*/
 var mapDesign;
-function loadMap(mapType){ 
-  if(mapDesign == undefined)
-  {
+function loadMap(val){ 
+    if(mapDesign == undefined)
+    {
     mapDesign = new MapDesign();
-    console.log("DEFINE MAPDESIGN");
-  }
-  mapDesign.removeLayers();
-  mapDesign.isSourceReady(mapType);
+    
+    }
+    mapDesign.removeLayers();
+    while (mapDesign.isSourceReady(val) == false) {}
 }
+
+//function to load thematic maps for UI wizard
+function loadMapTypes() {
+    loadMap(1);
+    loadMap(2);
+    loadMap(3);
+    loadMap(4);
+    setTimeout(function () {
+        mapDesign.concreteMapArray[0].map.setTarget("mapType_DotDensity");
+        mapDesign.concreteMapArray[1].map.setTarget("mapType_Heatmap");
+        mapDesign.concreteMapArray[2].map.setTarget("mapType_Choropleth");
+        mapDesign.concreteMapArray[3].map.setTarget("mapType_PropSymbol");
+    }, 8000);
+    // setTimeout(function () {
+    //     loadMap(4);
+    // }, 6800);
+    // setTimeout(function () {
+    //     loadMap(3);
+    // }, 10000);
+    // setTimeout(function () {
+    // }, 14000);
+}
+
+
+
+
 
 function loadDataset(url, callback){
     var feat;
@@ -33,64 +59,44 @@ function changeDataset(val){
     {
         loadDataset('http://localhost:8080/geoserver/wfs?service=WFS&' +
           'version=1.1.0&request=DescribeFeatureType&typename=GIS:households&' +
-          'outputFormat=application/json&srsname=EPSG:4326&maxFeatures=1', changeAttributes);
+          'outputFormat=application/json&srsname=EPSG:4326&maxFeatures=1', changeAtt);
     }
 }
 
-function changeAttributes(feat){
+function changeAtt(feat){
     var obj = feat.responseText.substring(feat.responseText.indexOf("[{\"name\""),feat.responseText.lastIndexOf("}]}"));
     var arr = JSON.parse(obj);
-    for(var i = 1; i < arr.length; i++)
+
+  for(var i = 1; i < arr.length; i++)
     {
         for(key in arr[i])
         {
             if(key == 'name')
             {
                 var val = arr[i][key];
-                console.log(val);
-                document.getElementById("tablebody").innerHTML += "<tr><td><input type=\"checkbox\" value=\"" + val + "\"" + "></td><td>"
-                + val + 
-                "</td></tr>";
+                document.getElementById("attr").innerHTML += " <option>" + val + "</option><br>";
             }
         }
     }
 }
-
-// Receive the getCapabilities request
-function receiveDataSets(url, callback){
-    var feat;
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-            callback(this);
-        }
-    };
-    xmlHttp.open("GET", url,true);
-    xmlHttp.send(null);
-}
-
-// Request capabilities which contain the names of datasets
-function requestCapabilities(){
-    receiveDataSets('http://localhost:8080/geoserver/wfs?service=WFS&version=1.1.0&request=GetCapabilities&outputFormat=application/json',
-        listDataSets);
-}
-
-//This uses jQuery which is to be added in the html :  
-function listDataSets(response){
-    var obj = response.responseText;
-    var featureDatabases = []
-    var xmlDoc = $.parseXML(obj);
-    var $xml = $(xmlDoc);
-    var $featureDB = $xml.find("FeatureType");
-    $featureDB.each(function(){
-        featureDatabases.push($(this).find("Name").text());
-    });
-    console.log(featureDatabases);
-    return featureDatabases;
-}
-
-
 /* Citations
   A: https://gis.stackexchange.com/questions/167112/how-to-create-a-circle-in-openlayers-3
   B: https://stackoverflow.com/questions/25924762/is-it-possible-to-identify-all-the-feature-layer-inside-of-other-layer
 */
+
+$(document).ready(function(){
+   $("#attr").change(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var button = $("#attrNext").next("button");
+    console.log("this.value: " + this.value);
+    if (this.value == "1") {
+      button.prop("disabled", true);   
+      console.log("disabled");    
+    } else {
+      button.prop("disabled", false)
+      console.log("enabled");
+      $('#attrNext').removeAttr('disabled');
+    };
+  });
+})
