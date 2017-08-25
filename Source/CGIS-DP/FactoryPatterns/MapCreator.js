cc2f11bf8bfb94c4102c765ce3035e29640cc280
Cian Steenkamp <ciansteenkamp@gmail.com>
2017-08-25 13:13:17 +0200

    Clean Up

--- Source/CGIS-DP/FactoryPatterns/MapCreator.js
+++
@@ -1,6 +0,0 @@
-var MapCreator = function() {
-    if (this.constructor === MapCreator) {
-        throw new Error("Can't instantiate abstract class!, type must be specified");
-    }
-    this.tileLayer;
-};
