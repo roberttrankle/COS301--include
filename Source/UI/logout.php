4582a07d4f295db1166075e8eb42360469b25b0c
Cian Steenkamp <ciansteenkamp@gmail.com>
2017-08-25 13:11:15 +0200

    Clean Up

--- Source/UI/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if(isset($_SESSION['username'])){
-		session_destroy();
-		echo "logging out";
-		header("location: proto-SideMenu.html");
-	}
-	header("location: proto-SideMenu.html");
-?>
--- Source/UI/readme.md
+++
@@ -1,1 +0,0 @@
-UI of CGIS map production
